931cb67755253544d069f89a57c3c28f9056e09a
20063914 <20063914@wit.ie>
2017-09-12 11:05:29 +0100

    Revision of Unity

---
+++ Week1/Assets/Scripts/Main.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	int score;
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print("Score <" + score + ">");
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		// print("Collided with " + hit.gameObject.name);
+		if (hit.gameObject.tag == "coin") {
+			Destroy(hit.gameObject);
+			score++;
+		}
+	}
+}
